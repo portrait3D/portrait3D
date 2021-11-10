3306270b5b43ea925513e1222da53a434eaa1f79
Benjamin Provost <prob2702@usherbrooke.ca>
2021-11-10 13:35:02 -0500

    Retrait d'export en ply et sauvegarde automatique à la racine de la solution avec génération de nom automatique

--- portrait3d/portrait3d/Exporter.cs
+++ portrait3d/portrait3d/Exporter.cs
@@ -5,7 +5,6 @@ namespace Portrait3D
     using Microsoft.Kinect.Toolkit.Fusion;
     using System.IO;
     using System.Globalization;
-    using Microsoft.Win32;
     using System.Collections.ObjectModel;
 
     /// <summary>
@@ -13,6 +12,10 @@ namespace Portrait3D
     /// </summary>
     class Exporter
     {
+        // Name of the file containing the next name to use for the next export file
+        private const string DirectoryPath = "..\\..\\..\\";
+        private const string ExportNameFileName = "exportName.txt";
+
         /// <summary>
         /// Save mesh in ASCII Wavefront .OBJ file
         /// </summary>
@@ -69,67 +72,6 @@ namespace Portrait3D
         }
 
         /// <summary>
-        /// Save mesh in ASCII .PLY file with per-vertex color
-        /// </summary>
-        /// <param name="mesh">Calculated mesh object</param>
-        /// <param name="writer">The text writer</param>
-        private static void SaveAsciiPlyMesh(Mesh mesh, TextWriter writer)
-        {
-            if (null == mesh || null == writer)
-            {
-                return;
-            }
-
-            var vertices = mesh.GetVertices();
-            var indices = mesh.GetTriangleIndexes();
-
-            // Check mesh arguments
-            if (0 == vertices.Count || 0 != vertices.Count % 3 || vertices.Count != indices.Count)
-            {
-                throw new ArgumentException(Properties.Resources.InvalidMeshArgument);
-            }
-
-            int faces = indices.Count / 3;
-
-            // Write the PLY header lines
-            writer.WriteLine("ply");
-            writer.WriteLine("format ascii 1.0");
-            writer.WriteLine("comment file created by Microsoft Kinect Fusion");
-
-            writer.WriteLine("element vertex " + vertices.Count.ToString(CultureInfo.InvariantCulture));
-            writer.WriteLine("property float x");
-            writer.WriteLine("property float y");
-            writer.WriteLine("property float z");
-
-            writer.WriteLine("element face " + faces.ToString(CultureInfo.InvariantCulture));
-            writer.WriteLine("property list uchar int vertex_index");
-            writer.WriteLine("end_header");
-
-            var centers = GetMeshXZCenters(vertices);
-
-            // Sequentially write the 3 vertices of the triangle, for each triangle
-            for (int i = 0; i < vertices.Count; i++)
-            {
-                var vertex = vertices[i];
-
-                string vertexString = (vertex.X - centers.Item1).ToString(CultureInfo.InvariantCulture) + " " + (-vertex.Y).ToString(CultureInfo.InvariantCulture) + " " + (-vertex.Z + centers.Item2).ToString(CultureInfo.InvariantCulture);
-
-                writer.WriteLine(vertexString);
-            }
-
-            // Sequentially write the 3 vertex indices of the triangle face, for each triangle, starts at 0
-            for (int i = 0; i < faces; i++)
-            {
-                string baseIndex0 = (i * 3).ToString(CultureInfo.InvariantCulture);
-                string baseIndex1 = ((i * 3) + 1).ToString(CultureInfo.InvariantCulture);
-                string baseIndex2 = ((i * 3) + 2).ToString(CultureInfo.InvariantCulture);
-
-                string faceString = "3 " + baseIndex0 + " " + baseIndex1 + " " + baseIndex2;
-                writer.WriteLine(faceString);
-            }
-        }
-
-        /// <summary>
         /// Calculates the center point of the model as the middle between the farthest X coordinates and Z coordinates
         /// </summary>
         /// <param name="vertex">List of vertices of the mesh</param>
@@ -153,47 +95,22 @@ namespace Portrait3D
             return new Tuple<float, float>((maxX - minX) / 2.0f, (maxZ - minZ) / 2.0f);
         }
 
-
         /// <summary>
         /// Generates a save file dialog and saves based on the extension selected.
         /// </summary>
         /// <param name="mesh">Calculated mesh object</param>
         public static void ExportMeshToFile(Mesh mesh)
         {
-            Stream stream;
-            SaveFileDialog saveFileDialog = new SaveFileDialog
-            {
-                FileName = "Export.stl",
-                Filter = "OBJ file (*.obj)|*.obj|PLY file (*.ply)|*.ply",
-                DefaultExt = "obj",
-                AddExtension = true
-            };
+            string exportFileName = File.Exists(DirectoryPath + ExportNameFileName) ? File.ReadAllText(DirectoryPath + ExportNameFileName) : "0";
+            exportFileName = (int.Parse(exportFileName) + 1).ToString();
 
-            if (saveFileDialog.ShowDialog() ?? false)
-            {
-                if ((stream = saveFileDialog.OpenFile()) != null)
-                {
-                    //Fetch file extension
-                    string extension = saveFileDialog.SafeFileName.Split('.')[1];
+            Stream stream = File.OpenWrite(DirectoryPath + exportFileName + ".obj");
+            StreamWriter streamWriter = new StreamWriter(stream);
 
-                    //Write per file extension
-                    switch (extension)
-                    {
-                        case "obj":
-                            SaveAsciiObjMesh(mesh, new StreamWriter(stream));
-                            break;
+            SaveAsciiObjMesh(mesh, streamWriter);
+            streamWriter.Close();
 
-                        case "ply":
-                            SaveAsciiPlyMesh(mesh, new StreamWriter(stream));
-                            break;
-
-                        default:
-                            stream.Close();
-                            throw new Exception("Extension not recognized");
-                    }
-                    stream.Close();
-                }
-            }
+            File.WriteAllText(DirectoryPath + ExportNameFileName, exportFileName);
         }
     }
 }
