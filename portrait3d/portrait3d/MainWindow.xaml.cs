f7a64a9a12609e3c5abf17d4c68aed562ead0b84
Benjamin Provost <prob2702@usherbrooke.ca>
2021-12-01 11:58:16 -0500

    Ajout du controle de la precision pour le traitement du model

--- portrait3d/portrait3d/MainWindow.xaml.cs
+++ portrait3d/portrait3d/MainWindow.xaml.cs
@@ -12,7 +12,6 @@ using Microsoft.Kinect.Toolkit.Fusion;
 #nullable enable
 namespace Portrait3D
 {
-    /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window, IDisposable
