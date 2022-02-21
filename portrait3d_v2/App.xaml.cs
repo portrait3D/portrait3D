37d181e44c1ef25e8e532207e376e699db452f3a
Benjamin Provost <benprovost98@gmail.com>
2022-02-21 11:07:05 -0500

    Ajout du projet pour la kinect v2

---
+++ portrait3d_v2/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="App.xaml.cs" company="Microsoft">
+//     Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+namespace Portrait3D.Portrait3D_v2
+{
+    using System.Windows;
+
+    /// <summary>
+    /// The implementation of the App class.
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
