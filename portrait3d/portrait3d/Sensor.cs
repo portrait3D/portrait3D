3a6e96bf9ca75eab69712b6d84939d3c3a98e3da
Étienne Lacasse <lace3304@usherbrooke.ca>
2022-04-22 12:36:37 -0400

    elevation angle + name

--- portrait3d/portrait3d/Sensor.cs
+++ portrait3d/portrait3d/Sensor.cs
@@ -84,7 +84,7 @@ namespace Portrait3D
                     return Properties.Resources.NoKinectReady;
                 }
                 sensor.Start();
-                sensor.ElevationAngle = 5;
+                sensor.ElevationAngle = 0;
             }
             catch (IOException ex)
             {
